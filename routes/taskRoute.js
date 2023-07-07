213acdb54ed2a012bee122b105d485b82bcb5654
Hafizur Rahman <99hafizurrahman@gmail.com>
2023-07-07 13:23:54 +0530

    protect routes added

--- routes/taskRoute.js
+++ routes/taskRoute.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const protect = require("../middleware/authMiddleware");
 const router = express.Router();
 const {
   createTask,
@@ -11,10 +12,10 @@ const {
 // router.route("/").get(getTasks).post(createTask);
 // router.route("/:id").get(getTask).delete(deleteTask).put(updateTask);
 
-router.post("/", createTask);
-router.get("/", getTasks);
-router.get("/:id", getTask);
-router.delete("/:id", deleteTask);
-router.put("/:id", updateTask);
+router.post("/", protect, createTask);
+router.get("/", protect, getTasks);
+router.get("/:id", protect, getTask);
+router.delete("/:id", protect, deleteTask);
+router.put("/:id", protect, updateTask);
 
 module.exports = router;
